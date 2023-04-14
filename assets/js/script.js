2647acb25270648b808822bd178dcc3b00ce1682
Chase Miller <chasehmiller97@gmail.com>
2023-04-13 22:30:06 -0400

    updated javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
+var count = 5;
 var timerEl = document.getElementById('clock');
 var incrementEl = document.querySelector("#add-time");
 var decrementEl = document.querySelector("#lose-time");
+var countEl = document.querySelector("#seconds");
+var scoreEl = document.querySelector("#score");
+var userNameSpan = document.querySelector("#user-name");
+var userScoreSpan = document.querySelector("#user-score");
 
 function clock() {
     var timeLeft = 60;
@@ -16,6 +21,59 @@ function clock() {
         }
     }, 1000);
 
+    incrementEl.addEventListener("correct", function() {
+        count++;
+        clock();
+    })
+
+    decrementEl.addEventListener("wrong", function() {
+        if (count > 0){
+            count--;
+            clock();
+        }
+    })
+}
+
+function grade() {
+    var baseLine = 0;
+
+    var scoreInterval = setInterval(function (){
+        if (baseLine > 1){
+            scoreInterval = baseLine;
+            baseLine--; 
+        } else {
+            scoreEl.textContent = '';
+            clearInterval(scoreInterval);
+            displayMessage()
+        }
+    })
+
+    incrementEl.addEventListener("correct", function() {
+        count++;
+        grade();
+    })
+
+    decrementEl.addEventListener("wrong", function () {
+        if (grade > 0){
+            count--;
+            grade();
+        }
+    })
+}
+
+
+function leaderBoard() {
 
 }
 
+function renderLastRegistered() {
+    var name = localStorage.getItem("name");
+    var scoring = localStorage.getItem("display-score");
+
+    if (!name || !scoring) {
+        return;
+    }
+
+    userNameSpan.textContent = name;
+    userScoreSpan.textContent = scoring;
+}
